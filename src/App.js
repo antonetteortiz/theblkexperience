a220f04479348b31aa7e7c0e779ea36703ab23bb
Antonette_Ortiz <ORTIZANTONETTE@GMAIL.COM>
2020-11-13 13:18:56 -0500

    updates to women shopping

--- src/App.js
+++ src/App.js
@@ -1,51 +1,76 @@
-import React, { useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import { Link, Route, Switch, Redirect } from "react-router-dom";
-import './App.css';
-import HomePage from "./Components/HomePage"
-import ShopMen from "./Components/ShopMen"
-import ShopWomen from "./Components/ShopWomen"
+import "./App.css";
+import HomePage from "./Components/HomePage";
+import ShopMen from "./Components/ShopMen";
+import ShopWomen from "./Components/ShopWomen";
 
 function App() {
   // In react hooks this is how we set state
   // The first agrument is the name of the state, second argument is how we manipulate that state
   // Anything passed in useState is the default value of that state
-  const [shopwomen, setShopWomen] = useState([])
+  const [shopwomen, setShopWomen] = useState([]);
   // UseEffect works similarily to componentDidMount
+  console.log("in App.js before useEffect");
   useEffect(() => {
-   let apiUrl = "https://sd-womens-fashion.herokuapp.com/WomenFashion";
-   fetch(apiUrl)
-   .then(data => data.json())
-   .then(shopwomen => setShopWomen(shopwomen))
-  //Empty array bracket will only run useEffect once, because we are fetching 
-  //Argument passed here is saying everytime our argument changes the API will be called.  
-  },[shopwomen]) 
+    console.log(`are we here`);
+    let apiUrl = "https://sd-womens-fashion.herokuapp.com/WomenFashion";
+    fetch(apiUrl)
+      .then((data) => {
+        console.log(data);
+        return data.json();
+      })
+      .then((shopwomen) => {
+        console.log(shopwomen);
+        setShopWomen(shopwomen);
+      })
+      .catch((error) => {
+        console.log(error);
+        console.log("uh oh");
+      });
+    //Empty array bracket will only run useEffect once, because we are fetching
+    //Argument passed here is saying everytime our argument changes the API will be called.
+  }, []);
 
-  return (      
-    <Switch>    
+  return (
+    <Switch>
+      <Route
+        exact
+        path="/"
+        render={() => {
+          return <HomePage />;
+        }}
+      />
+
+      <Route
+        // exact
+        path="/men"
+        render={() => {
+          return <ShopMen />;
+        }}
+      />
+
+      <Route
+        exact
+        path="/women"
+        render={() => {
+          return <ShopWomen womenBrands={shopwomen} />;
+        }}
+      />
+
+      {/* {shopwomen.length !== 0 ? (
         <Route
-            exact
-            path="/"
-            render={() => {
-              return <HomePage />;
-            }}
-          />
-
-          <Route
-            // exact
-            path="/men"
-            render={() => {
-              return <ShopMen />;
-            }}
-          />
-
-           <Route
-            // exact
-            path="/women"
-            render={() => {
-              return <ShopWomen />;
-            }}
-          />
-  </Switch>
+          exact
+          path="/shopwomen/:id"
+          render={(routerProps) => {
+            let shopwomen = shopwomen.filter(
+              (shopwomen) => shopwomen.id == routerProps.match.params.id
+            );
+            return <ShopWomen womenBrands={shopwomen} />;
+          }}
+        />
+      ) : null} */}
+    </Switch>
   );
 }
 
--- src/Components/ShopMen.js
+++ src/Components/ShopMen.js
@@ -1,7 +1,49 @@
 import React from "react";
 import Navbar from "./Navbar"
+import axios from "axios";
 
 function ShopMen() {
+  onst remove = (brand_name) => {
+        let encodedbrand_name = encodeURI(brand_name)
+        axios
+          .delete(`https://sd-womens-fashion.herokuapp.com/WomenFashion/${encodedbrand_name}`,
+            {
+              brand_name
+            }
+          )
+          .then(function (response) {
+            alert("Brand has been deleted!");
+            console.log(response);
+          });
+    }
+    
+ console.log(props.womenBrands)
+  let BrandList = props.womenBrands.map((shopwomen, i) => {
+    return (
+      <div className="col mb-4">
+        <div className="card" style={{ width: "18rem" }} key={i}>
+          <img src={shopwomen.logo} className="card-img-top" alt="..." />
+          <div className="card-body">
+            <h5 className="card-title">{shopwomen.brand_name}</h5>
+            <p className="card-text">{shopwomen.CEO}</p>
+            {/* <a
+              className="btn btn-primary"
+              href={`/updatebrand/${encodeURI(shopwomen.brand_name)}`}
+            >
+              Update
+            </a> */}
+            <a
+              className="btn btn-primary"
+              onClick={() => remove(shopwomen.brand_name)}
+            >
+              Delete
+            </a>
+          </div>
+        </div>
+      </div>
+    );
+  });
+
   return (
     <div>
     <Navbar />
