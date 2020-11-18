ca2cd3b921ada8aed1254c0cd0c16cf20e249091
Antonette_Ortiz <ORTIZANTONETTE@GMAIL.COM>
2020-11-18 10:27:04 -0500

    updated shop men's

--- src/Components/ShopMen.js
+++ src/Components/ShopMen.js
@@ -1,11 +1,10 @@
 import React from "react";
+import { Link, Route, Switch, Redirect, useHistory } from "react-router-dom";
 import Navbar from "./Navbar";
 import axios from "axios";
 import { Button } from "../stories/Button";
 import { Subscribe } from "../stories/Subscribe";
 import Forms from "../stories/Forms";
-import { Delete } from "../stories/Delete";
-import { Update } from "../stories/Update";
 
 function ShopMen(props) {
   const history = useHistory();
@@ -21,28 +20,37 @@ function ShopMen(props) {
       .then(function (response) {
         alert("Brand has been deleted!");
         console.log(response);
+        // history.push("/men")
+        window.location.href = `http://localhost:3000/men`;
       });
   };
 
   console.log(props.menBrands);
-  let BrandList = props.menBrands.map((shopmen, i) => {
+  let BrandList = props.menBrands.map((brand, i) => {
+    let encodedbrand_name = encodeURI(brand.brand_name);
+
+    console.log(`/men/brand/${encodedbrand_name}`);
     return (
       <div className="col mb-4">
         <div className="card" style={{ width: "auto", margin: "30px" }} key={i}>
-          <img
-            src="https://pds.exblog.jp/pds/1/201609/18/51/f0044151_165878.jpg"
-            className="img-fluid"
-            alt="Responsive image"
-          />
-          <div className="storybook-Delete">
-            <a href={`/updatebrand/${encodeURI(brand.brand_name)}`}>
-              <Update className="storybook-Update" label="Update" />
-            </a>
-            <Delete
-              className="storybook-delete"
-              onClick={() => remove(brand.brand_name)}
-              label="Delete"
+          <Link to={`/men/brand/${encodedbrand_name}`}>
+            <img
+              src={brand.logo}
+              className="img-fluid"
+              alt="Responsive image"
             />
+          </Link>
+
+          <div className="card-footer">
+            <a
+              className="card-link"
+              href={`/updatebrand/menswear/${encodeURI(brand.brand_name)}`}
+            >
+              Update
+            </a>
+            <a className="card-link" onClick={() => remove(brand.brand_name)}>
+              Delete
+            </a>
           </div>
         </div>
       </div>
